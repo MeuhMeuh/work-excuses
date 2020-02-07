0deb3021b369dad9a43fb2eddb9ff18d2c0eb321
Alexis Ménard <af.menard@gmail.com>
2020-02-07 17:00:54 +0100

    some more excuses

--- src/components/ExcuseSelector.vue
+++ src/components/ExcuseSelector.vue
@@ -22,7 +22,12 @@
                     'My boiler is out of service',
                     'My son is sick',
                     'My wife fall in the Loire river',
-                    'Someone stole my bike and destroyed my car'
+                    'Someone stole my bike and destroyed my car',
+                    'My dog ate my wife',
+                    'My son kicked my ass and I can't walk',
+                    'Sorry, I thought that it was a day off',
+                    'I forgot that I was working for you',
+                    'I am a "cadre", it's not about time but goals',
                 ],
                 excuse: ''
             };
